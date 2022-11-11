01e6667325a7413638abb55d69bc0a0201a35604
Seval Eduardo Sebastián <eseval@santafe.gov.ar>
2022-11-11 15:38:22 +0100

    docs: Modificar documentación

--- src/js/base.js
+++ src/js/base.js
@@ -7,35 +7,31 @@
 /**
  * Reconstruye el control Lista
  * @param {!string} id ID de la lista a reconstruir 
- * @returns {void}
  */
- function refreshSelect(id)
- {
-     $(`#${id}`).material_select();
-     $(".dropdown-content>li>span").css("color", 'black');
- }
- 
- /**
-  * Reconstruye los controles Lista 
-  * @returns {void}
-  */
-  function bootSelect()
-  {
-      $('select').each(function () {
-          refreshSelect(this.id);
-      });
-      $(".dropdown-content>li>span").css("color", 'black');
-  
-      /** 
-       * No se enganie mi entraniable lector ... este codigo esta para arreglar
-       * otro problema de Materialize. Eso es todo y nada mas. 
-       * 
-       * Problema: necesidad de hacer un click inicial en las listas 
-       * desplegables. 
-       */
-      setTimeout(function () {
-          $('.select-wrapper').each(function () {
-              this.addEventListener('click', e => e.stopPropagation())
-          });
-      }, 500); 
-  }
+function refreshSelect(id) {
+    $(`#${id}`).material_select();
+    $(".dropdown-content>li>span").css("color", 'black');
+}
+
+/**
+ * Reconstruye los controles Lista 
+ */
+function bootSelect() {
+    $('select').each(function () {
+        refreshSelect(this.id);
+    });
+    $(".dropdown-content>li>span").css("color", 'black');
+
+    /** 
+     * No se enganie mi entraniable lector ... este codigo esta para arreglar
+     * otro problema de Materialize. Eso es todo y nada mas. 
+     * 
+     * Problema: necesidad de hacer un click inicial en las listas 
+     * desplegables. 
+     */
+    setTimeout(function () {
+        $('.select-wrapper').each(function () {
+            this.addEventListener('click', e => e.stopPropagation())
+        });
+    }, 500);
+}
